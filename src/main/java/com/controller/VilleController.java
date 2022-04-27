409313d89176da0f0278d721bc69d52918ba5673
Zara Marks <zara.marks@reseau.eseo.fr>
2022-04-27 15:01:27 +0200

    init project

---
+++ src/main/java/com/controller/VilleController.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VilleController {
+
+	// fonction pour récupérer le contenu de la BDD
+	@RequestMapping(value="/ville", method=RequestMethod.GET)
+	public String get(@RequestParam(required  = false, value="codePostal") String codePostal) {
+		System.out.println("get");
+		// TODO : mon code vers la BDD
+
+		return "Todo";
+	}
+	
+	// TODO : 
+	// fonction pour enregistrer un element dans la BDD
+
+}
