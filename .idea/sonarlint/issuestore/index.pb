ebf4d22074755b67f4da2dd24dee0a5e24d62f00
Zara Marks <zara.marks@reseau.eseo.fr>
2022-04-30 00:02:59 +0200

    changes to allow cross origin + fix bugs

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,17 +1,5 @@
 
 R
 "src/main/java/com/Application.java,0/6/060e4465fa508961e9fd44875c5a850a9925ebfe
-7
-pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
 [
 +src/main/java/com/model/VilleListModel.java,e/8/e8918d9c8c8ff4946237bcfd44d5e6c35e06f332
-[
-+src/main/java/com/service/VilleService.java,0/6/0623addcb6ed814f8322797dcc0a6d4603845e91
-S
-#src/main/java/com/entity/Ville.java,0/4/047ef77f9822da10c48550dd83574f5f72c3bb83
-W
-'src/main/java/com/model/VilleModel.java,6/3/63fc81b297047bada55e1c0cc2521c86b1c69c75
-S
-#src/main/java/com/dao/VilleDao.java,f/4/f4eb0686112c5d02c374ac5910f17c051cf6731b
-a
-1src/main/java/com/controller/VilleController.java,6/1/611c7df8a29b39110bd1bd7f637f2a6a778ab026
--- src/main/java/com/controller/VilleController.java
+++ src/main/java/com/controller/VilleController.java
@@ -2,10 +2,14 @@ package com.controller;
 
 import com.dao.VilleDao;
 import com.entity.Ville;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.model.VilleModel;
 import com.service.VilleService;
 import javassist.tools.rmi.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,19 +24,27 @@ public class VilleController {
 	@Autowired
 	VilleService villeService;
 
-	// fonction pour récupérer le contenu de la BDD
-	@GetMapping("/all")
-	public ResponseEntity<List<Ville>> getAll() {
+	private static final Gson gson = new Gson();
 
-		return ResponseEntity.ok(villeService.getAll());
+
+	// fonction pour récupérer le contenu de la BDD
+	@CrossOrigin(origins = "http://localhost:8080")
+	@GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getAll() throws JsonProcessingException {
+
+		//gson.toJson(villeService.getAll())
+		ObjectMapper mapper = new ObjectMapper();
+
+		return gson.toJson(villeService.getAll());
 	}
 
 	@GetMapping("/{code}/code")
+	@CrossOrigin(origins = "http://localhost:8080")
 		public ResponseEntity<Ville> getByCode(@PathVariable("code") String code ) {
 
 			return ResponseEntity.ok(villeService.getByCode(code));
 		}
-
+	@CrossOrigin(origins = "http://localhost:8080")
 	@PostMapping
 	public ResponseEntity<Ville> add(@RequestBody VilleModel ville) throws SQLException {
 		System.out.println("entrou");
